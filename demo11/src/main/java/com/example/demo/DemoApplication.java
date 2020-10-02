490aca6c1352456af1d1728b699e8faceeb19f19
Ficko05 <filip-f@hotmail.com>
2020-10-02 11:05:32 +0200

    soap

---
+++ demo11/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        try {
+            String url = "http://wsgeoip.lavasoft.com/ipservice.asmx?op=GetLocation";
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "text/xml; charset=utf-8");
+            //String countryCode = "Canada";
+            String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                    "<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
+                    "  <soap:Body>\n" +
+                    "    <GetLocation xmlns=\"http://lavasoft.com/\" />\n" +
+                    "  </soap:Body>\n" +
+                    "</soap:Envelope>";
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(xml);
+            wr.flush();
+            wr.close();
+            String responseStatus = con.getResponseMessage();
+            System.out.println(responseStatus);
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println("response:" + response.toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+
